d2f0ceff3bbd61ce27f5d90ebd3a7867e20a464f
hongyipan152 <hongyipan152@gmail.com>
2026-02-27 16:00:54 +0000

    task1-fix

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,5 +2,5 @@ const { add } = require('../app/index');
 
 test('adds numbers correctly', () => {
   // Intentional failure for CI feedback
-  expect(add(1, 2)).toBe(4);
+  expect(add(1, 2)).toBe(3);
 });
